106542e64579d3b718a1a4da893cdcce29008d5f
Lucas Lopes <lp.skt25@gmail.com>
2025-08-08 00:36:18 -0300

    feat: Criação da estrutura inicial e dependencias

---
+++ src/main/java/com/lpdev/techbuddy/TechBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.lpdev.techbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechBuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechBuddyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lpdev/techbuddy/TechBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lpdev.techbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechBuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
